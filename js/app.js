ee1650edcd7fe699c35ce0f437d84cb1117ed855
Dominik Kopp <kopp340@gmail.com>
2026-07-24 22:06:02 +0200

    Release 0.2 - Live-Wetterdaten von GeoSphere

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const url = "https://dataset.api.hub.geosphere.at/v1/station/current/tawes-v1-10min?station_ids=11152&parameters=TL&parameters=FF&parameters=FFX&parameters=DD&parameters=RF&parameters=P&parameters=RR";
+
+async function loadWeather() {
+
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    const p = json.features[0].properties.parameters;
+
+    const temp = p.TL.data[0];
+
+    const wind = p.FF.data[0];
+
+    const direction = p.DD.data[0];
+
+    const gust = p.FFX.data[0];
+
+    const pressure = p.P.data[0];
+
+    const humidity = p.RF.data[0];
+
+    const rain = p.RR.data[0];
+
+    const time = json.timestamps[0];
+
+    document.getElementById("temperature").textContent =
+        temp.toFixed(1) + " °C";
+
+    document.getElementById("wind").textContent =
+        (wind * 1.94384).toFixed(1) + " kt";
+
+    document.getElementById("gust").textContent =
+        (gust * 1.94384).toFixed(1) + " kt";
+
+    document.getElementById("pressure").textContent =
+        pressure.toFixed(1) + " hPa";
+
+    document.getElementById("humidity").textContent =
+        humidity.toFixed(0) + " %";
+
+    document.getElementById("rain").textContent =
+        rain.toFixed(1) + " mm";
+
+    document.getElementById("direction").textContent =
+        direction + "°";
+
+    document.getElementById("timestamp").textContent =
+        time;
+}
+
+loadWeather();
